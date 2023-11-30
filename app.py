aaafdf629683eb634df0ea51b1eeedc74c2cab22
Diego-Machado-00 <61296868+Diego-Machado-00@users.noreply.github.com>
2023-11-30 04:15:59 +0000

    V2

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-#import joblib import load
+import pickle
 
 app = Flask(__name__)
-#model = load(open("models/model_perceptron.joblib"))
+model = pickle.load(open("models/model_perceptron.pkl", "rb"))
 
 @app.route("/")
 def home():
@@ -10,10 +10,22 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    #prediction = model.predict()
-    #prediction = 1 if prediction == 1 else -1
-    #return render_template("index.html", prediction=prediction)
-    return 'hola mundo'
+    age = request.form.get("Age")
+    sex = request.form.get("Sex")
+    physActivity = request.form.get("PhysActivity")
+    fruits = request.form.get("Fruits")
+    veggies = request.form.get("Veggies")
+    hvyAlcoholConsump = request.form.get("HvyAlcoholConsump")
+    smoker = request.form.get("Smoker")
+    highBP = request.form.get("HighBP")
+    highChol = request.form.get("HighChol")
+    bMI = request.form.get("BMI")
+    genHlth = request.form.get("GenHlth")
+    physHlthad = request.form.get("PhysHlth")
+    diffWalk = request.form.get("DiffWalk")
+    heartDiseaseorAttack = request.form.get("HeartDiseaseorAttack")
+    prediction = model.predict([[age, sex, physActivity, fruits, veggies, hvyAlcoholConsump, smoker, highBP, highChol, bMI, genHlth, physHlthad, diffWalk, heartDiseaseorAttack]])
+    return render_template("index.html", predictions = prediction)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
