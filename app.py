9e8115de735595ac3f7eb72a161099d4689aeda6
Diego-Machado-00 <61296868+Diego-Machado-00@users.noreply.github.com>
2023-12-01 03:27:21 +0000

    V3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 
 app = Flask(__name__)
@@ -26,6 +26,26 @@ def predict():
     heartDiseaseorAttack = request.form.get("HeartDiseaseorAttack")
     prediction = model.predict([[age, sex, physActivity, fruits, veggies, hvyAlcoholConsump, smoker, highBP, highChol, bMI, genHlth, physHlthad, diffWalk, heartDiseaseorAttack]])
     return render_template("index.html", predictions = prediction)
+
+@app.route('/api/predict', methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)  # Get data posted as a json
+    age = data['Age']
+    sex = data['Sex']
+    physActivity = data['PhysActivity']
+    fruits = data['Fruits']
+    veggies = data['Veggies']
+    hvyAlcoholConsump = data['HvyAlcoholConsump']
+    smoker = data['Smoker']
+    highBP = data['HighBP']
+    highChol = data['HighChol']
+    bMI = data['BMI']
+    genHlth = data['GenHlth']
+    physHlthad = data['PhysHlth']
+    diffWalk = data['DiffWalk']
+    heartDiseaseorAttack = data['HeartDiseaseorAttack']
+    prediction = model.predict([[age, sex, physActivity, fruits, veggies, hvyAlcoholConsump, smoker, highBP, highChol, bMI, genHlth, physHlthad, diffWalk, heartDiseaseorAttack]])
+    return jsonify({'prediction': prediction}) 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
